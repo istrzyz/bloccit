970157ce85e1487ed204f332b7d469951f3cc508
Ian Strzyzewski <istrzyz@gmail.com>
2015-10-29 13:24:23 -0700

    Finished Ckpt32

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,58 +2,55 @@
  include RandomData
 
  RSpec.describe PostsController, type: :controller do
-
    let (:my_post) {Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+
    describe "GET #index" do
      it "returns http success" do
        get :index
        expect(response).to have_http_status(:success)
      end
-   end
 
-   it "assigns [my_post] to @posts" do
-     get :index
+     it "assigns [my_post] to @posts" do
+       get :index
 
-     expect(assigns(:posts)).to eq([my_post])
-   end
- end
+       expect(assigns(:posts)).to eq([my_post])
+     end
+   end # GET index
 
-
-     describe "GET new" do
-       it "returns http success" do
-           get :new
-           expect(response).to have_http_status(:success)
-       end
-
-       it "renders the #new view" do
+   describe "GET new" do
+     it "returns http success" do
          get :new
-         expect(response).to render_template :new
-       end
-
-       it "instantiates @post" do
-         get :new
-         expect(assigns(:post)).not_to be_nil
-       end
+         expect(response).to have_http_status(:success)
      end
 
-     describe "POST create" do
-
-       it "increases the number of Post by 1" do
-         expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
-       end
-
-       it "assigns the new post to @post" do
-         post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-         expect(assigns(:post)).to eq Post.last
-       end
-
-       it "redirects to new post" do
-         post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-         expect(resonse).to redirect_to Post.last
-       end
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
      end
 
-     describe "GET show" do
+     it "instantiates @post" do
+       get :new
+       expect(assigns(:post)).not_to be_nil
+     end
+   end
+
+   describe "POST create" do
+
+     it "increases the number of Post by 1" do
+       expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+     end
+
+     it "assigns the new post to @post" do
+       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+       expect(assigns(:post)).to eq Post.last
+     end
+     it "redirects to the new post" do
+       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+       expect(response).to redirect_to Post.last
+     end
+   end
+
+   describe "GET show" do
      it "returns http success" do
  # #16
        get :show, {id: my_post.id}
@@ -71,3 +68,4 @@
        expect(assigns(:post)).to eq(my_post)
      end
    end
+end
