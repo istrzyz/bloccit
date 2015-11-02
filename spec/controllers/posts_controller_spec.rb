cb05fe5587029962f39ecb83749f66fe1f8d2e19
Ian Strzyzewski <istrzyz@gmail.com>
2015-11-02 13:54:44 -0800

    Changing branch

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,7 +17,6 @@
 
      expect(assigns(:posts)).to eq([my_post])
    end
- end
 
 
      describe "GET new" do
@@ -56,43 +55,41 @@
 
      describe "GET show" do
      it "returns http success" do
- # #16
        get :show, {id: my_post.id}
        expect(response).to have_http_status(:success)
      end
      it "renders the #show view" do
- # #17
        get :show, {id: my_post.id}
        expect(response).to render_template :show
      end
 
      it "assigns my_post to @post" do
        get :show, {id: my_post.id}
- # #18
        expect(assigns(:post)).to eq(my_post)
      end
    end
 
-   describe "GET edit" do
-     it "returns http success" do
-       get :edit, {id: my_post.id}
-       expect(response).to have_http_status(:success)
-     end
+     describe "GET edit" do
+       it "returns http success" do
+         get :edit, {id: my_post.id}
+         expect(response).to have_http_status(:success)
+       end
 
-     it "renders the #edit view" do
-       get :edit, {id: my_post.id}
+       it "renders the #edit view" do
+         get :edit, {id: my_post.id}
 
-       expect(response).to render_template :edit
-     end
+         expect(response).to render_template :edit
+       end
 
 
-     it "assigns post to be updated to @post" do
-       get :edit, {id: my_post.id}
+       it "assigns post to be updated to @post" do
+         get :edit, {id: my_post.id}
 
-       post_instance = assigns(:post)
+         post_instance = assigns(:post)
 
-       expect(post_instance.id).to eq my_post.id
-       expect(post_instance.title).to eq my_post.title
-       expect(post_instance.body).to eq my_post.body
+         expect(post_instance.id).to eq my_post.id
+         expect(post_instance.title).to eq my_post.title
+         expect(post_instance.body).to eq my_post.body
+       end
      end
    end
