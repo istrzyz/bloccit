b258156271fb88910ca1f0eb27fc3ab1f8ef4497
Ian Strzyzewski <istrzyz@gmail.com>
2015-11-10 17:36:49 -0800

    Ckpt 36 attempt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104015749) do
+ActiveRecord::Schema.define(version: 20151110033326) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -40,12 +40,28 @@ ActiveRecord::Schema.define(version: 20151104015749) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
