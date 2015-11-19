953f59e8070afe8525910b03b644dfbf6679e3a7
Ian Strzyzewski <istrzyz@gmail.com>
2015-11-19 12:17:22 -0800

    Assignment 37 Attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,14 @@ class UsersController < ApplicationController
       flash[:error] = "There was an error creating your account. Please try again."
       render :new
     end
+
+    def confirm
+      @user = User.new(user_params)
+    end
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
