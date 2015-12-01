5ed412ab0e6699a076e06a8c9a52f88667bd9e4e
Ian Strzyzewski <istrzyz@gmail.com>
2015-11-30 22:39:37 -0800

    Did ckpt39 from ckpt38 branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 include RandomData
 
-# Create topics
+5.times do
+  user = User.create!(
+
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -12,6 +21,7 @@ topics = Topic.all
 50.times do
 
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -28,7 +38,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'istrzyz@gmail.com', # replace this with your personal email
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
