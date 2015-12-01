0055e215ce888936f5fdfbb79dafc1fcfed6bcd7
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-01 12:21:16 -0800

    Ckpt40

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,11 +38,18 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'istrzyz@gmail.com', # replace this with your personal email
-  password: 'helloworld'
-)
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
 puts "Seed finished"
 puts "#{User.count} users created"
