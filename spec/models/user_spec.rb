44b3b92b983268ab9789be7ec6277fb8b76ab401
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-01 17:18:45 -0800

    attempt assignment 40

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe User, type: :model do
       it "should respond to member?" do
         expect(user).to respond_to(:member?)
       end
+
+      it "should respond to moderator?" do
+        expect(user).to respond_to(:moderator?)
+      end
     end
 
     describe "roles" do
@@ -48,6 +52,24 @@ RSpec.describe User, type: :model do
       context "member user" do
         it "should return true for #member?" do
           expect(user.member?).to be_truthy
+        end
+
+        it "should return false for moderator?" do
+          expect(user.moderator?).to be_falsey
+        end
+
+        it "should return false for #admin?" do
+          expect(user.admin?).to be_falsey
+        end
+      end
+
+      context "moderator user" do
+        it "should return false for #member?" do
+          expect(user.member?).to be_falsey
+        end
+
+        it "should return true for #moderator?" do
+          expect(user.moderator?).to be_truthy
         end
 
         it "should return false for #admin?" do
@@ -62,6 +84,10 @@ RSpec.describe User, type: :model do
 
         it "should return fase for #member?" do
           expect(user.member?).to be_falsey
+        end
+
+        it "should return false for #moderator?"
+          expect(user.moderator?).to be_falsey
         end
 
         it "should return true for #admin?" do
