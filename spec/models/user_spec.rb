9be63d136c98956f62577e6a00bde1556826fc16
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-06 22:51:09 -0800

    Ckpt44 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include RandomData
 
 
 RSpec.describe User, type: :model do
@@ -6,6 +7,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:posts)}
     it { should have_many(:comments)}
     it { should have_many(:votes)}
+    it { should have_many(:favorites)}
 
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(1) }
@@ -73,22 +75,36 @@ RSpec.describe User, type: :model do
     end
 
     describe "invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-    let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
+      let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+      let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+      let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
 
-    it "should be an invalid user due to blank name" do
-      expect(user_with_invalid_name).to_not be_valid
+      it "should be an invalid user due to blank name" do
+        expect(user_with_invalid_name).to_not be_valid
+      end
+
+      it "should be an invalid user due to blank email" do
+        expect(user_with_invalid_email).to_not be_valid
+      end
+
+      it "should be an invalid user due to incorrectly formatted email address" do
+        expect(user_with_invalid_email_format).to_not be_valid
+      end
     end
 
-    it "should be an invalid user due to blank email" do
-      expect(user_with_invalid_email).to_not be_valid
+    describe "#favorite_for(post)" do
+      before do
+        topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+        @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      end
+
+      it "returns `nil` if the user has not favorited the post" do
+        expect(user.favorite_for(@post)).to be_nil
+      end
+
+      it "returns the appropriate favorite if it exists" do
+        favorite = user.favorites.where(post: @post).create
+        expect(user.favorite_for(@post)).to eq(favorite)
+      end
     end
-
-    it "should be an invalid user due to incorrectly formatted email address" do
-      expect(user_with_invalid_email_format).to_not be_valid
-    end
-
-  end
-
 end
