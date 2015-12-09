fb4a6259a58bc4be2e49c452a694770492a374c0
Ian Strzyzewski <istrzyz@gmail.com>
2015-12-08 17:11:31 -0800

    attempted

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -63,10 +63,6 @@ RSpec.describe UsersController, type: :controller do
   describe "not signed in" do
     let(:factory_user) { create(:user) }
 
-    before do
-      post :create, user: new_user_attributes
-    end
-
     it "returns http success" do
       get :show, {id: factory_user.id}
       expect(response).to have_http_status(:success)
